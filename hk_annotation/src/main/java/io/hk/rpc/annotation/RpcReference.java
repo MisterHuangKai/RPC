08458df4ba8b20645199e19d8da48e8dc615464e
MisterHuangKai <812875941@qq.com>
2023-07-11 16:17:24 +0800

    冰河rpc, 第1、2、3、4章: 服务提供者注解@RpcService, 服务消费者注解@RpcReference, 通用扫描器.

---
+++ hk_annotation/src/main/java/io/hk/rpc/annotation/RpcReference.java
@@ -0,0 +1,232 @@
+/**
+ * Copyright 2020-9999 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.hk.rpc.annotation;
+
+import io.hk.rpc.constants.RpcConstants;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author binghe
+ * @version 1.0.0
+ * @description rpc服务消费者，配置优先级：服务消费者字段上配置的@RpcReference注解属性 > yml文件 > @RpcReference默认注解属性
+ * @HK注:
+ * 1.服务消费者的注解@RpcReference是标注到字段上的.
+ * 2.@Service
+ *   public class ConsumerServiceImpl implements ConsumerService{
+ *       @RpcReference(registryType="zookeeper",registryAddress="127.0.0.1:2181",loadBalanceType="zkconsistenthash",version="1.0.0",group="binghe",
+ *       serializationType="protostuff",proxy="cglib",timeout=30000,async=true,oneway=false)
+ *       private BingheService bingheService;
+ *   }
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+@Autowired
+public @interface RpcReference {
+
+    /**
+     * 版本号
+     */
+    String version() default RpcConstants.RPC_COMMON_DEFAULT_VERSION;
+
+    /**
+     * 注册中心类型, 目前的类型包含：zookeeper、nacos、etcd、consul
+     */
+    String registryType() default RpcConstants.RPC_REFERENCE_DEFAULT_REGISTRYTYPE;
+
+    /**
+     * 注册地址
+     */
+    String registryAddress() default RpcConstants.RPC_REFERENCE_DEFAULT_REGISTRYADDRESS;
+
+    /**
+     * 负载均衡类型，默认基于ZK的一致性Hash
+     */
+    String loadBalanceType() default RpcConstants.RPC_REFERENCE_DEFAULT_LOADBALANCETYPE;
+
+    /**
+     * 序列化类型，目前的类型包含：protostuff、kryo、json、jdk、hessian2、fst
+     */
+    String serializationType() default RpcConstants.RPC_REFERENCE_DEFAULT_SERIALIZATIONTYPE;
+
+    /**
+     * 超时时间，默认5s
+     */
+    long timeout() default RpcConstants.RPC_REFERENCE_DEFAULT_TIMEOUT;
+
+    /**
+     * 是否异步执行
+     */
+    boolean async() default false;
+
+    /**
+     * 是否单向调用
+     */
+    boolean oneway() default false;
+
+    /**
+     * 代理的类型，jdk：jdk代理， javassist: javassist代理, cglib: cglib代理
+     */
+    String proxy() default RpcConstants.RPC_REFERENCE_DEFAULT_PROXY;
+
+    /**
+     * 服务分组，默认为空
+     */
+    String group() default RpcConstants.RPC_COMMON_DEFAULT_GROUP;
+
+    /**
+     * 心跳间隔时间，默认30秒
+     */
+    int heartbeatInterval() default RpcConstants.RPC_COMMON_DEFAULT_HEARTBEATINTERVAL;
+
+    /**
+     * 扫描空闲连接间隔时间，默认60秒
+     */
+    int scanNotActiveChannelInterval() default RpcConstants.RPC_COMMON_DEFAULT_SCANNOTACTIVECHANNELINTERVAL;
+
+    /**
+     * 重试间隔时间
+     */
+    int retryInterval() default RpcConstants.RPC_REFERENCE_DEFAULT_RETRYINTERVAL;
+
+    /**
+     * 重试间隔时间
+     */
+    int retryTimes() default RpcConstants.RPC_REFERENCE_DEFAULT_RETRYTIMES;
+
+    /**
+     * 是否开启结果缓存
+     */
+    boolean enableResultCache() default false;
+
+    /**
+     * 缓存结果的时长，单位是毫秒
+     */
+    int resultCacheExpire() default RpcConstants.RPC_SCAN_RESULT_CACHE_EXPIRE;
+
+
+    /**
+     * 是否开启直连服务
+     */
+    boolean enableDirectServer() default false;
+
+    /**
+     * 直连服务的地址
+     */
+    String directServerUrl() default RpcConstants.RPC_COMMON_DEFAULT_DIRECT_SERVER;
+
+    /**
+     * 是否开启延迟连接
+     */
+    boolean enableDelayConnection() default false;
+
+    /**
+     * 默认并发线程池核心线程数
+     */
+    int corePoolSize() default RpcConstants.DEFAULT_CORE_POOL_SIZE;
+
+    /**
+     * 默认并发线程池最大线程数
+     */
+    int maximumPoolSize() default RpcConstants.DEFAULT_MAXI_NUM_POOL_SIZE;
+
+
+    /**
+     * 流控分析类型
+     */
+    String flowType() default RpcConstants.FLOW_POST_PROCESSOR_PRINT;
+
+
+    /**
+     * 是否开启缓冲区
+     */
+    boolean enableBuffer() default false;
+
+    /**
+     * 缓冲区大小
+     */
+    int bufferSize() default RpcConstants.DEFAULT_BUFFER_SIZE;
+
+
+    /**
+     * 容错class
+     */
+    Class<?> fallbackClass() default void.class;
+
+    /**
+     * 容错class名称
+     */
+    String fallbackClassName() default RpcConstants.DEFAULT_FALLBACK_CLASS_NAME;
+
+    /**
+     * 反射类型
+     */
+    String reflectType() default RpcConstants.DEFAULT_REFLECT_TYPE;
+
+    /**
+     * 是否开启限流
+     */
+    boolean enableRateLimiter() default false;
+
+    /**
+     * 限流类型
+     */
+    String rateLimiterType() default RpcConstants.DEFAULT_RATELIMITER_INVOKER;
+
+    /**
+     * 在milliSeconds毫秒内最多能够通过的请求个数
+     */
+    int permits() default RpcConstants.DEFAULT_RATELIMITER_PERMITS;
+
+    /**
+     * 毫秒数
+     */
+    int milliSeconds() default RpcConstants.DEFAULT_RATELIMITER_MILLI_SECONDS;
+
+    /**
+     * 当限流失败时的处理策略
+     */
+    String rateLimiterFailStrategy() default RpcConstants.RATE_LIMILTER_FAIL_STRATEGY_DIRECT;
+
+    /**
+     * 是否开启熔断策略
+     */
+    boolean enableFusing() default false;
+    /**
+     * 熔断规则标识
+     */
+    String fusingType() default RpcConstants.DEFAULT_FUSING_INVOKER;
+
+    /**
+     * 在fusingMilliSeconds毫秒内触发熔断操作的上限值
+     */
+    double totalFailure() default RpcConstants.DEFAULT_FUSING_TOTAL_FAILURE;
+
+    /**
+     * 熔断的毫秒时长
+     */
+    int fusingMilliSeconds() default RpcConstants.DEFAULT_FUSING_MILLI_SECONDS;
+
+
+    /**
+     * 异常处理类型
+     */
+    String exceptionPostProcessorType() default RpcConstants.EXCEPTION_POST_PROCESSOR_PRINT;
+}
---
+++ hk_annotation/src/main/java/io/hk/rpc/annotation/RpcService.java
@@ -0,0 +1,64 @@
+package io.hk.rpc.annotation;
+
+import io.hk.rpc.constants.RpcConstants;
+import org.springframework.stereotype.Component;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author binghe
+ * @version 1.0.0
+ * @description bhrpc服务提供者注解
+ * @HK注:
+ * 1.@RpcService(interfaceClass=BingheService.class, version="1.0.0", group="binghe")
+ * public class BingheServiceImpl implements BingheService{
+ * }
+ * 2.@RpcService(interfaceClassName="io.binghe.rpc.demo.service.BingheService", version="1.0.0", group="binghe")
+ * public class BingheServiceImpl implements BingheService{
+ * }
+ * 3.服务提供者注解@RpcService是标注到实现类上的
+ */
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Component
+public @interface RpcService {
+
+    /**
+     * 接口的Class
+     */
+    Class<?> interfaceClass() default void.class;
+
+    /**
+     * 接口的ClassName
+     */
+    String interfaceClassName() default "";
+
+    /**
+     * 版本号
+     */
+    String version() default RpcConstants.RPC_COMMON_DEFAULT_VERSION;
+
+    /**
+     * 服务分组，默认为空
+     */
+    String group() default RpcConstants.RPC_COMMON_DEFAULT_GROUP;
+
+    /**
+     * 权重
+     */
+    int weight() default 0;
+
+    /**
+     * 心跳间隔时间，默认30秒
+     */
+    int heartbeatInterval() default RpcConstants.RPC_COMMON_DEFAULT_HEARTBEATINTERVAL;
+
+    /**
+     * 扫描空闲连接间隔时间，默认60秒
+     */
+    int scanNotActiveChannelInterval() default RpcConstants.RPC_COMMON_DEFAULT_SCANNOTACTIVECHANNELINTERVAL;
+
+}
