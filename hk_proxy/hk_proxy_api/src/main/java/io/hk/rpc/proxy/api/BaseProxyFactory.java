89f1fd51d87293870d630eacac197f96eb50a97f
Huang <812875941@qq.com>
2024-05-20 23:28:35 +0800

    冰河rpc,第20章:服务消费者动态代理扩展优化。 content:⭐运用了工厂模式生成动态代理,具体可见ProxyFactory、BaseProxyFactory、JdkProxyFactory在RpcClient.create(...)中的运用。

--- hk_proxy/hk_proxy_api/src/main/java/io/hk/rpc/proxy/api/BaseProxyFactory.java
+++ hk_proxy/hk_proxy_api/src/main/java/io/hk/rpc/proxy/api/BaseProxyFactory.java
@@ -2,6 +2,8 @@ package io.hk.rpc.proxy.api;
 
 import io.hk.rpc.proxy.api.config.ProxyConfig;
 import io.hk.rpc.proxy.api.object.ObjectProxy;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * 基础代理工厂类
@@ -11,10 +13,13 @@ import io.hk.rpc.proxy.api.object.ObjectProxy;
  */
 public abstract class BaseProxyFactory<T> implements ProxyFactory {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(BaseProxyFactory.class);
+
     protected ObjectProxy<T> objectProxy;
 
     @Override
     public <T> void init(ProxyConfig<T> proxyConfig) {
+        LOGGER.info("BaseProxyFactory: abstract class init method.");
         this.objectProxy = new ObjectProxy(proxyConfig.getClazz(),
                 proxyConfig.getServiceVersion(),
                 proxyConfig.getServiceGroup(),
--- hk_proxy/hk_proxy_api/src/main/java/io/hk/rpc/proxy/api/ProxyFactory.java
+++ hk_proxy/hk_proxy_api/src/main/java/io/hk/rpc/proxy/api/ProxyFactory.java
@@ -2,6 +2,8 @@ package io.hk.rpc.proxy.api;
 
 import io.hk.rpc.proxy.api.config.ProxyConfig;
 import io.hk.rpc.spi.annotation.SPI;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * 代理工厂接口
@@ -12,6 +14,8 @@ import io.hk.rpc.spi.annotation.SPI;
 @SPI
 public interface ProxyFactory {
 
+    Logger LOGGER = LoggerFactory.getLogger(ProxyFactory.class);
+
     /**
      * 获取代理对象
      */
@@ -21,7 +25,7 @@ public interface ProxyFactory {
      * 默认初始化方法
      */
     default <T> void init(ProxyConfig<T> proxyConfig) {
-
+        LOGGER.info("ProxyFactory init.");
     }
 
 }
