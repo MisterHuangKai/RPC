26ec91b00408143a2ece9fbf1f2657edd7c6b34f
MisterHuangKai <812875941@qq.com>
2024-05-29 18:53:37 +0800

    25

---
+++ hk_spi/src/main/java/io/hk/rpc/spi/factory/ExtensionFactory.java
@@ -0,0 +1,24 @@
+package io.hk.rpc.spi.factory;
+
+import io.hk.rpc.spi.annotation.SPI;
+
+/**
+ * 扩展类加载器的工厂接口
+ *
+ * @author HuangKai
+ * @date 2024/5/29
+ */
+@SPI("spi")
+public interface ExtensionFactory {
+
+    /**
+     * 获取扩展类对象
+     *
+     * @param <T>   泛型类型
+     * @param key   传入的key值
+     * @param clazz Class类型对象
+     * @return 扩展类对象
+     */
+    <T> T getExtension(String key, Class<T> clazz);
+
+}
