a1238e6bcb8d74507aa2db43f49b202343685c89
MisterHuangKai <812875941@qq.com>
2024-06-20 18:45:08 +0800

    21,zookeeper测试

--- hk_test/hk_test_registry/src/main/java/io/hk/rpc/test/registry/ZookeeperRegistryTest.java
+++ hk_test/hk_test_registry/src/main/java/io/hk/rpc/test/registry/ZookeeperRegistryTest.java
@@ -4,8 +4,14 @@ import io.hk.rpc.protocol.meta.ServiceMeta;
 import io.hk.rpc.registry.api.RegistryService;
 import io.hk.rpc.registry.api.config.RegistryConfig;
 import io.hk.rpc.registry.zookeeper.ZookeeperRegistryService;
+import org.apache.zookeeper.CreateMode;
+import org.apache.zookeeper.KeeperException;
+import org.apache.zookeeper.ZooDefs;
+import org.apache.zookeeper.ZooKeeper;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.io.IOException;
 
 /**
  * Zookeeper注册测试
@@ -44,6 +50,17 @@ public class ZookeeperRegistryTest {
     @Test
     public void testDestroy() throws Exception {
         this.registryService.destroy();
+    }
+
+
+    // 网上找到的其他写法, 仅供了解。因为会报错: ERROR ClientCnxn:532 - Error while calling watcher, 但是插入zookeeper成功
+    public static void main(String[] args) throws IOException, InterruptedException, KeeperException {
+        ZooKeeper zooKeeper = new ZooKeeper("47.103.9.3:2181", 6000, null);
+        Thread.sleep(1000);
+        if (zooKeeper.exists("/test1", false) == null) {
+            zooKeeper.create("/test1", "znode1".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+        }
+        System.out.println(new String(zooKeeper.getData("/test1", false, null)));
     }
 
 }
