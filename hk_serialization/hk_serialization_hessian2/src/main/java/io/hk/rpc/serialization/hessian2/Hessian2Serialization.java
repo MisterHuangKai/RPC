fcab5a18674c32774046a2ac197bc044da267543
Huang <812875941@qq.com>
2024-06-24 23:13:21 +0800

    28

--- hk_serialization/hk_serialization_hessian2/src/main/java/io/hk/rpc/serialization/hessian2/Hessian2Serialization.java
+++ hk_serialization/hk_serialization_hessian2/src/main/java/io/hk/rpc/serialization/hessian2/Hessian2Serialization.java
@@ -1,5 +1,6 @@
 package io.hk.rpc.serialization.hessian2;
 
+import com.caucho.hessian.io.Hessian2Input;
 import com.caucho.hessian.io.Hessian2Output;
 import io.hk.rpc.common.exception.SerializerException;
 import io.hk.rpc.serialization.api.Serialization;
@@ -7,7 +8,9 @@ import io.hk.rpc.spi.annotation.SPIClass;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 
 /**
  * Hessian2序列化与反序列化
@@ -15,7 +18,7 @@ import java.io.ByteArrayOutputStream;
 @SPIClass
 public class Hessian2Serialization implements Serialization {
 
-    private static Logger logger = LoggerFactory.getLogger(Hessian2Serialization.class);
+    private final Logger logger = LoggerFactory.getLogger(Hessian2Serialization.class);
 
     /**
      * 序列化
@@ -29,11 +32,27 @@ public class Hessian2Serialization implements Serialization {
             throw new SerializerException("serialize object is null.");
         }
         byte[] bytes = new byte[0];
-        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-        Hessian2Output hessian2Output = new Hessian2Output(outputStream);
-
-
-        return new byte[0];
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        Hessian2Output hessian2Output = new Hessian2Output(byteArrayOutputStream);
+        try {
+            hessian2Output.startMessage();
+            hessian2Output.writeObject(obj);
+            hessian2Output.flush();
+            hessian2Output.completeMessage();
+            bytes = byteArrayOutputStream.toByteArray();
+        } catch (IOException e) {
+            throw new SerializerException(e.getMessage(), e);
+        } finally {
+            try {
+                if (hessian2Output != null) {
+                    hessian2Output.close();
+                    byteArrayOutputStream.close();
+                }
+            } catch (IOException e) {
+                throw new SerializerException(e.getMessage(), e);
+            }
+        }
+        return bytes;
     }
 
     /**
@@ -44,6 +63,30 @@ public class Hessian2Serialization implements Serialization {
      */
     @Override
     public <T> T deserialize(byte[] data, Class<T> clazz) {
-        return null;
+        logger.info("execute hessian2 deserialize...");
+        if (data == null) {
+            throw new SerializerException("deserialize data is null.");
+        }
+        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(data);
+        Hessian2Input hessian2Input = new Hessian2Input(byteArrayInputStream);
+        T obj = null;
+        try {
+            hessian2Input.startMessage();
+            obj = (T) hessian2Input.readObject();
+            hessian2Input.completeMessage();
+        } catch (IOException e) {
+            throw new SerializerException(e.getMessage(), e);
+        } finally {
+            try {
+                if (null != hessian2Input) {
+                    hessian2Input.close();
+                    byteArrayInputStream.close();
+                }
+            } catch (IOException e) {
+                throw new SerializerException(e.getMessage(), e);
+            }
+        }
+        return obj;
     }
+
 }
