e017c54e652cbec72866080fd105ae72df05bcc1
MisterHuangKai <812875941@qq.com>
2024-06-29 14:49:49 +0800

    冰河rpc,第30章: 基于SPI扩展Kryo序列化与反序列化机制 content:增加KryoSerialization实现类;增加对应的META-INF下特定路径的配置文件。 亮点: Kryo与Hessian相比,Kryo序列化后的大小更小,且在处理已经序列化过的类时速度更快;提供了简单易用的API.

--- hk_serialization/hk_serialization_kryo/src/main/java/io/hk/rpc/serialization/kryo/KryoSerialization.java
+++ hk_serialization/hk_serialization_kryo/src/main/java/io/hk/rpc/serialization/kryo/KryoSerialization.java
@@ -1,21 +1,53 @@
 package io.hk.rpc.serialization.kryo;
 
 
+import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryo.io.Input;
+import com.esotericsoftware.kryo.io.Output;
+import com.esotericsoftware.kryo.serializers.JavaSerializer;
+import io.hk.rpc.common.exception.SerializerException;
 import io.hk.rpc.serialization.api.Serialization;
+import io.hk.rpc.spi.annotation.SPIClass;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 
 /**
- * @param
- * @author HuangKai
- * @date 2024/6/25
- * @return
+ * Kryo序列化与反序列化
  */
+@SPIClass
 public class KryoSerialization implements Serialization {
+
+    private final Logger logger = LoggerFactory.getLogger(KryoSerialization.class);
+
     /**
      * 序列化
      */
     @Override
     public <T> byte[] serialize(T obj) {
-        return new byte[0];
+        logger.info("execute kryo serialize...");
+        if (obj == null) {
+            throw new SerializerException("serialize object is null.");
+        }
+        Kryo kryo = new Kryo();
+        kryo.setReferences(false);
+        kryo.register(obj.getClass(), new JavaSerializer());
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        Output output = new Output(outputStream);
+        kryo.writeClassAndObject(output, obj);
+        output.flush();
+        output.close();
+        byte[] bytes = outputStream.toByteArray();
+        try {
+            outputStream.flush();
+            outputStream.close();
+        } catch (IOException e) {
+            throw new SerializerException(e.getMessage(), e);
+        }
+        return bytes;
     }
 
     /**
@@ -23,6 +55,15 @@ public class KryoSerialization implements Serialization {
      */
     @Override
     public <T> T deserialize(byte[] data, Class<T> clazz) {
-        return null;
+        logger.info("execute kryo deserialize...");
+        if (data == null) {
+            throw new SerializerException("deserialize object is null.");
+        }
+        Kryo kryo = new Kryo();
+        kryo.setReferences(false);
+        kryo.register(clazz, new JavaSerializer());
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(data);
+        Input input = new Input(inputStream);
+        return (T) kryo.readClassAndObject(input);
     }
 }
